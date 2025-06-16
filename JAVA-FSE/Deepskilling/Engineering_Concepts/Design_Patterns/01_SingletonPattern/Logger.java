cdc0b05c261ae21ec5b77855d0ac2cd73bbbcd7a
Narmadha103 <119487274+Narmadha103@users.noreply.github.com>
2025-06-16 14:16:38 +0530

    Added Exercise 1: Sigleton Pattern

--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/01_SingletonPattern/Logger.java
+++ JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/01_SingletonPattern/Logger.java
@@ -1,14 +1,11 @@
 public class Logger {
 
-    // Private static instance of Logger
     private static Logger singleInstance;
 
-    // Private constructor to prevent external instantiation
     private Logger() {
         System.out.println("Logger Initialized.");
     }
 
-    // Public static method to get the singleton instance
     public static Logger getInstance() {
         if (singleInstance == null) {
             singleInstance = new Logger();
@@ -16,7 +13,7 @@ public class Logger {
         return singleInstance;
     }
 
-    // Method to log messages
+    
     public void logMessage(String message) {
         System.out.println("LOG: " + message);
     }
--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/01_SingletonPattern/Main.java
+++ JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/01_SingletonPattern/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Get the first instance of Logger
+        
         Logger loggerOne = Logger.getInstance();
         loggerOne.logMessage("First log entry");
 
-        // Get the second instance of Logger
         Logger loggerTwo = Logger.getInstance();
         loggerTwo.logMessage("Second log entry");
 
-        // Validate that both references are the same
+       
         if (loggerOne == loggerTwo) {
             System.out.println("Both logger instances are the same.");
         } else {
