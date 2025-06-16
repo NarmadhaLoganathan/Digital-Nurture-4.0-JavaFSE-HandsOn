8d47c571853022f6393f91ad078d30894661dfc3
Narmadha103 <119487274+Narmadha103@users.noreply.github.com>
2025-06-16 14:48:17 +0530

    Added Exercise 6: Proxy  Pattern

--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/06_ProxyPattern/RealImage.java
+++ JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/06_ProxyPattern/RealImage.java
@@ -1,11 +1,11 @@
 public class RealImage implements Image {
     private String fileName;
-
+  
     public RealImage(String fileName) {
         this.fileName = fileName;
         loadImageFromServer();
     }
-
+//Method
     private void loadImageFromServer() {
         System.out.println("Loading image from remote server: " + fileName);
     }
