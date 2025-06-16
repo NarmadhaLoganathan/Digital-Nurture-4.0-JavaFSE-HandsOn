763a3d01f0d0aea507e17210dae5d5c559a7b152
Narmadha103 <119487274+Narmadha103@users.noreply.github.com>
2025-06-16 14:38:12 +0530

    Added Exercise 4: Adapter  Pattern

--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/04_AdapterPattern/EmailService.java
+++
@@ -1,5 +0,0 @@
-public class EmailService {
-    public void sendEmail(String msg) {
-        System.out.println("Email sent: " + msg);
-    }
-}
--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/04_AdapterPattern/SmsService.java
+++
@@ -1,5 +0,0 @@
-public class SmsService {
-    public void sendText(String msg) {
-        System.out.println("SMS sent: " + msg);
-    }
-}
