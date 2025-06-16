fc7528d3909ed9d689c7baafc6cd537bc3129ec3
Narmadha103 <119487274+Narmadha103@users.noreply.github.com>
2025-06-16 12:52:43 +0530

    Added Exercise 3: Builder Pattern

---
+++ JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/07_ObserverPattern/ObserverTest.java
@@ -0,0 +1,14 @@
+public class ObserverTest {
+    public static void main(String[] args) {
+        StockMarket market = new StockMarket();
+
+        Observer mobile = new MobileApp("Narmadha's Phone");
+        Observer web = new WebApp("StockWatch");
+
+        market.register(mobile);
+        market.register(web);
+
+        market.setStock("TCS", 3520.75);
+        market.setStock("INFY", 1423.50);
+    }
+}
