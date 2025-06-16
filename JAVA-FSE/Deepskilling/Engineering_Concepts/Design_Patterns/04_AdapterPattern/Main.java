fe91e74a87e35c2176530d7394f33724c81e5472
Narmadha103 <119487274+Narmadha103@users.noreply.github.com>
2025-06-16 14:38:38 +0530

    Added Exercise 4: Adapter  Pattern

--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/04_AdapterPattern/Main.java
+++ JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/04_AdapterPattern/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Using PayPal through Adapter
+       
         PaymentProcessor paypal = new PayPalAdapter(new PayPalGateway());
         paypal.processPayment(1000);
 
-        // Using Stripe through Adapter
+        
         PaymentProcessor stripe = new StripeAdapter(new StripeGateway());
         stripe.processPayment(1500);
 
-        // Using Razorpay through Adapter
         PaymentProcessor razorpay = new RazorpayAdapter(new RazorpayGateway());
         razorpay.processPayment(2000);
     }
