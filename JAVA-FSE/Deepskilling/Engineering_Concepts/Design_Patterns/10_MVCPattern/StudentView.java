a4ab49332f739dbbb40cf30352f6e0d31c6629cd
Narmadha103 <119487274+Narmadha103@users.noreply.github.com>
2025-06-17 15:16:15 +0530

    Solved error

--- JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/10_MVCPattern/StudentView.java
+++ JAVA-FSE/Deepskilling/Engineering_Concepts/Design_Patterns/10_MVCPattern/StudentView.java
@@ -1,5 +1,5 @@
-public class MemberView {
-    public void displayMemberDetails(String name, String memberId, String role) {
+public class StudentView {
+    public void displayStudentDetails(String name, String memberId, String role) {
         System.out.println("==== Club Member Details ====");
         System.out.println("Name     : " + name);
         System.out.println("ID       : " + memberId);
