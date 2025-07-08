7c73f328e4e6ed912ecc3865f47fcc3382bcd77d
Narmadha L <narmadhalv05@gmail.com>
2025-07-08 13:00:19 +0530

    added Handson3

--- JAVA-FSE-DEEPSKILLING/week3_Spring/Spring_Data-JPA-with-Hibernate/3.spring-data-jpa-handson/Handson3/src/main/java/com/example/ormlearn/OrmLearnApplication.java
+++ JAVA-FSE-DEEPSKILLING/week3_Spring/Spring_Data-JPA-with-Hibernate/3.spring-data-jpa-handson/Handson3/src/main/java/com/example/ormlearn/OrmLearnApplication.java
@@ -1,30 +1,12 @@
+
 package com.example.ormlearn;
 
-import com.example.ormlearn.model.Employee;
-import com.example.ormlearn.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.List;
 
 @SpringBootApplication
-public class OrmLearnApplication implements CommandLineRunner {
-
-    @Autowired
-    private EmployeeService employeeService;
-
+public class OrmLearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrmLearnApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        List<Employee> employees = employeeService.getAllPermanentEmployees();
-        employees.forEach(e -> {
-            System.out.println("Employee: " + e.getName());
-            System.out.println("Department: " + e.getDepartment().getName());
-            System.out.println("Skills: " + e.getSkillList());
-        });
     }
 }
