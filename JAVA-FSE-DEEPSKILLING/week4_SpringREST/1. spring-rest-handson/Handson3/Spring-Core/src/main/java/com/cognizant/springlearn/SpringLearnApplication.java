27ba1feeb7ecd753d6c30827aaa7bc9a4ae05099
Narmadha L <narmadhalv05@gmail.com>
2025-07-11 21:55:59 +0530

    added Handson3

---
+++ JAVA-FSE-DEEPSKILLING/week4_SpringREST/1. spring-rest-handson/Handson3/Spring-Core/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,33 @@
+package com.cognizant.springlearn;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        SpringLearnApplication app = new SpringLearnApplication();
+        app.displayDate();
+    }
+
+    public void displayDate() {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        try {
+            Date date = format.parse("31/12/2018");
+            LOGGER.debug("Parsed Date: {}", date);
+        } catch (ParseException e) {
+            LOGGER.error("Date parsing failed", e);
+        }
+        LOGGER.info("END");
+    }
+}
